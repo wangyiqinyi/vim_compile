08898d16cb77abd4c00c7b9a381d6ec4b8c846dd
wangyiqinyi <wangyiqinyi@163.com>
2017-08-15 22:25:48 +0800

    fix build_msvc.bat

--- build_msvc.bat
+++ build_msvc.bat
@@ -1,10 +1,11 @@
-@ECHO OFF
+REM @ECHO OFF
 
 REM set some useful evironment variables
 REM ------------------------------------
-SET VIM_SRC=%~dp0
-SET VIM_BIN_OUPUT=E:\Vim
-REM SET ZIP_DIR=%VIM_SRC%zip
+SET CUR_DIR=%~dp0
+SET VIM_SRC=%CUR_DIR%\vim
+SET VIM_BIN_OUTPUT=%CUR_DIR%\bin
+REM SET ZIP_DIR=%CUR_DIR%\zip
 
 REM set library versions ...
 REM ------------------------------------
@@ -15,20 +16,25 @@ REM REM set LIBPERLVER=520
 REM set LIBTCLSHRT=86
 REM set LIBTCLLONG=8.6
 
+
 REM clean up and get source code from upstream
 REM ------------------------------------
-CD /D %VIM_SRC%
+IF EXIST %VIM_BIN_OUTPUT% rmdir %VIM_BIN_OUTPUT% /s
+IF NOT EXIST %VIM_BIN_OUTPUT% mkdir %VIM_BIN_OUTPUT%
+
+REM CD /D %VIM_SRC%
+REM git submodule update --recursive --remote
 
 REM begin to build ...
 REM ------------------------------------
 CD /D %VIM_SRC%\src
-IF NOT EXIST temp\win64 mkdir temp\win64
 
 REM prepare the build environment ...
 REM those evironment variables can be
 REM found in file "%VIM_SRC%\src\Make_mvc.mak"
 REM ------------------------------------
-call "E:\Microsoft Visual Studio 2017\VC\Auxiliary\Build\vcvarsall.bat" amd64
+SET VC_VAR_DIR=E:\Microsoft Visual Studio 2017\VC\Auxiliary\Build
+call "%VC_VAR_DIR%\vcvarsall.bat" amd64
 SET CPU=AMD64
 SET DEBUG=no
 SET FEATURES=HUGE
@@ -44,7 +50,7 @@ SET OPTIMIZE=MAXSPEED
 REM SET LUA=C:\Dev\Utils\lua
 REM SET DYNAMIC_LUA=yes
 REM SET LUA_VER=%LIBLUASHRT%
-SET PYTHON=E:\Python27
+SET PYTHON=E:\Python27\
 SET DYNAMIC_PYTHON=no
 SET PYTHON_VER=%LIBPYTHON2%
 REM SET PYTHON3=C:\Dev\Python35
@@ -58,7 +64,7 @@ REM SET TCL_VER=%LIBTCLSHRT%
 REM SET TCL_VER_LONG=%LIBTCLLONG%
 REM SET DYNAMIC_TCL=yes
 REM SET XPM=xpm\x64
-SET SDK_INCLUDE_DIR=C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Include
+SET SDK_INCLUDE_DIR=C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Include\
 SET MSVCVER=15.0
 
 REM compile! (x64)
@@ -76,30 +82,30 @@ nmake /C /S /f Make_mvc.mak IME=no GIME=no GUI=no OLE=no DIRECTX=no
 
 REM keep up the right directory structure
 REM -------------------------------------
-robocopy ..\runtime\ temp\win64 /MIR
-copy ..\vimtutor.* temp\win64
-copy xxd\xxd.exe temp\win64
-copy *.exe temp\win64
-copy vimtbar.dll temp\win64
-copy README.txt temp\win64
+robocopy ..\runtime\ %VIM_BIN_OUTPUT% /MIR
+copy ..\vimtutor.* %VIM_BIN_OUTPUT%
+copy xxd\xxd.exe %VIM_BIN_OUTPUT%
+copy *.exe %VIM_BIN_OUTPUT%
+copy vimtbar.dll %VIM_BIN_OUTPUT%
+copy README.txt %VIM_BIN_OUTPUT%
 
-mkdir temp\win64\GVimExt
-mkdir temp\win64\VisVim
+mkdir %VIM_BIN_OUTPUT%\GVimExt
+mkdir %VIM_BIN_OUTPUT%\VisVim
 
-copy gvimext\*.dll temp\win64\gvimext\
-copy gvimext\*.inf temp\win64\gvimext\
-copy gvimext\*.reg temp\win64\gvimext\
-copy gvimext\README.txt temp\win64\gvimext\
+copy gvimext\*.dll %VIM_BIN_OUTPUT%\gvimext\
+copy gvimext\*.inf %VIM_BIN_OUTPUT%\gvimext\
+copy gvimext\*.reg %VIM_BIN_OUTPUT%\gvimext\
+copy gvimext\README.txt %VIM_BIN_OUTPUT%\gvimext\
 
-copy VisVim\*.txt temp\win64\VisVim\
-copy VisVim\*.dll temp\win64\VisVim\
-copy VisVim\*.bat temp\win64\VisVim\
+copy VisVim\*.txt %VIM_BIN_OUTPUT%\VisVim\
+copy VisVim\*.dll %VIM_BIN_OUTPUT%\VisVim\
+copy VisVim\*.bat %VIM_BIN_OUTPUT%\VisVim\
 
 REM cleanup
 REM ------------------------------------
-del temp\win64\vimlogo.*
-del temp\win64\*.png
-del temp\win64\vimE:\Python27??x??.*
+del %VIM_BIN_OUTPUT%\vimlogo.*
+del %VIM_BIN_OUTPUT%\*.png
+del %VIM_BIN_OUTPUT%\vim??x??.*
 
 REM pack it!
 REM 7z command line options:
@@ -110,14 +116,13 @@ REM -mmt[N] -- set number of cpu threads
 REM -mx=9 -- Level 9 of LZMA2 compression method for 7z format
 REM for more options, please check the help file shipped with 7-Zip distribution
 REM ------------------------------------
-REM cd temp\win64
+REM cd %VIM_BIN_OUTPUT%
 REM set 7Z_BIN=E:\7-Zip\7z.exe
 REM 7Z_BIN a -mx=9 -mmt4 -r -bd %ZIP_DIR%\vim-win64.7z * 
 REM clean up everything
 REM in the source code directory
 REM ------------------------------------
-REM MOVE temp %VIM_BIN_OUPUT%
-CD /D %VIM_SRC%
+CD /D %CUR_DIR%
 
 EXIT /B
 @ECHO ON
